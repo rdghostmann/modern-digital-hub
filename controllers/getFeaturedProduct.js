46e3de765360c9131f9a5cf0c59ef1d36f35fdc0
unknown <randalchukzwilson@gmail.com>
2025-05-28 12:57:37 +0100

    Update IX

--- controllers/getFeaturedProduct.js
+++ controllers/getFeaturedProduct.js
@@ -5,7 +5,7 @@ import { connectToDB } from "@/lib/connectDB"
 
 export async function getFeaturedProducts() {
   await connectToDB()
-  const products = await Product.find().limit(3).lean()
+  const products = await Product.find().limit(4).lean()
    return products.map(product => {
       const { _id, ...rest } = product
       return {
