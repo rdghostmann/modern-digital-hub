498e8ab75e1ae43a184a4ebe3e0463d6ae3938a6
unknown <randalchukzwilson@gmail.com>
2025-06-09 16:33:09 +0100

    Added i

--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -31,7 +31,6 @@ export async function POST(req) {
       password: hashedPassword,
       phone,
       role: "user", // Default role
-      status: "active", // Default status
     });
 
     return NextResponse.json({
